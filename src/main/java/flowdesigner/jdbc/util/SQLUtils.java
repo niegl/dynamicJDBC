541aab68756ed27a2fc82fd2407591c4ab960b88
广苓 聂 <174878698@qq.com>
2022-10-29 15:38:57 +0800

    1、添加 通过jar包获取驱动类型的接口，用来在驱动配置页面显示驱动名称；

--- src/main/java/flowdesigner/jdbc/util/SQLUtils.java
+++ src/main/java/flowdesigner/jdbc/util/SQLUtils.java
@@ -3,11 +3,23 @@ package flowdesigner.jdbc.util;
 import com.alibaba.druid.DbType;
 import com.alibaba.druid.sql.parser.Lexer;
 import com.alibaba.druid.sql.parser.SQLParserUtils;
+import commonUtility.file.FileKit;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.EmptyFileFilter;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 public class SQLUtils{
+    public SQLUtils() {
+    }
 
     public static Set<String> getKeywords(DbType dbType) {
         Lexer lexer = SQLParserUtils.createLexer("select *", dbType);
@@ -22,6 +34,24 @@ public class SQLUtils{
     public static String getKeywordsAsString(DbType dbType) {
         Set<String> keywords = getKeywords(dbType);
         return StringUtils.join(keywords,",");
+    }
+
+    public static ArrayList<String> searchDriverClassName(List<String> files) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (String file : files) {
+            Collection<File> jarFiles = FileUtils.listFiles(new File(file),
+                    FileFilterUtils.and(new SuffixFileFilter("jar"), EmptyFileFilter.NOT_EMPTY),
+                    DirectoryFileFilter.INSTANCE);
+            jarFiles.forEach(jarFile -> {
+                String className = FileKit.readFileFromJar( jarFile,"java.sql.Driver");
+                if (!className.isEmpty()) {
+                    strings.add(className);
+                }
+            });
+        }
+
+        return strings;
     }
 
 }
